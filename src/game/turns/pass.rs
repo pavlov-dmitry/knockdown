343ceef633df143e2d3a6d36cedc940b7f1ab84b
pavlov-dmitry <voidwalker@mail.ru>
2019-12-21 17:14:28 +0300

    Переделал пас на общую функцию поворота

--- src/game/turns/pass.rs
+++ src/game/turns/pass.rs
@@ -3,12 +3,7 @@ use super::EventsBuilder;
 use super::GameState;
 
 pub fn pass<S: GameState + EventsBuilder>(game: &mut S, player_id: Id) {
-    let player = game.get_player(player_id);
-    let target = game.get_player_target(player_id);
-    let angle = player.position.angle_to(&target.position);
-    if angle != player.angle {
-        game.player_rotate(player_id, angle);
-    }
+    super::turn_on_target_if_need(game, player_id, false);
 }
 
 #[cfg(test)]
